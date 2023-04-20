c737ea21b289fe2bce4139fd035f09fccdf0a543
Veda Nandikam <nandikam.v@northeastern.edu>
2023-04-20 05:04:10 +0000

    final

--- pages/HR Employee Dashboard/jsobjects/Utils/Utils.js
+++
@@ -1,8 +0,0 @@
-export default {
-	myVar1: [],
-	myVar2: {},
-	updateStatus: () => {
-		storeValue('tableIndex',application_table.selectedRowIndex)
-		updateApplicationStatus.run(() => getApplications.run(), () => {})
-	}
-}
